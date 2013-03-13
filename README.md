76589232e91bc0b74104f1a87c3839fd724f2fba
Erik Sunde <esu@enonic.com>
2013-03-13 21:00:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+A scripting toolkit to ease management of Enonic CMS
+
+=== Scripts ====
+src/cms-shell/contextParser.groovy = creates a cms-shell script from a context.xml file that creates a jdbc connection from the database resource, initialises the upgrade process and does a upgrade check.
---
+++ src/cms-shell/contextParser.groovy
@@ -0,0 +1,32 @@
+
+/**
+ * Created by Erik Sunde ( esu At enonic.com ) March 2013
+ * Script to generate a cms-shell to configure a jdbc connections, set upgrade parameters and do a 'upgrade check'
+ */
+
+
+def contextParser( def contextFilename, def cmsHomeDir, def jdbcInitFilename )
+{
+    def context = new XmlParser().parse(new File(contextFilename))
+    def resource = context.get("Resource")
+
+    def jdbcInitFile = new File(jdbcInitFilename)
+
+    jdbcInitFile.write("jdbc add --name database --url \""+resource."@url"[0] +" --user "+resource."@username"[0]+" --password "+resource."@password"[0] +"\n")
+    jdbcInitFile.append("upgrade init --connector database --homeDir "+cmsHomeDir+" --webApp ../webapp/webapp.war\n")
+    jdbcInitFile.append("upgrade check\n")
+}
+
+def help()
+{
+    println("This script creates a scriptfile for cms-shell to configure a jdbc connections, set upgrade parameters and do a 'upgrade check'")
+    println("Usage:")
+    println("contextParser.groovy <context.xml> <cms.home dir> <em-shell script filename>")
+    System.exit(2)
+}
+
+if ( args.length == 3 && new File(args[0]).isFile() && new File(args[1]).isDirectory() )
+{
+    contextParser(args[0], args[1], args[2])
+}
+
