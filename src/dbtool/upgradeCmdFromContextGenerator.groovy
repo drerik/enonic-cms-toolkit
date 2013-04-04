3b2985fee9881e50c596d5e2f6436f2bb0d94f6b
Erik Sunde <esu@enonic.com>
2013-04-04 12:38:22 +0200

    added script to generate dbtool upgrade command

---
+++ src/dbtool/upgradeCmdFromContextGenerator.groovy
@@ -0,0 +1,34 @@
+#!/usr/bin/env groovy
+
+/**
+ * Created by Erik Sunde ( esu At enonic.com ) April 2013
+ * Script to generate a dbtool upgrade command for 4.4 and 4.5
+ */
+
+
+def contextParser( def contextFilename, def cmsHomeDir, def jdbcInitFilename )
+{
+    def context = new XmlParser().parse(new File(contextFilename))
+    def resource = context.get("Resource")
+
+    def jdbcInitFile = new File(jdbcInitFilename)
+
+
+    println("./upgradetool.sh -a "+resource."@username"[0]+":"+resource."@password"[0] +" -d org.postgresql.Driver -u \""+resource."@url"[0] +"\" -h "+cmsHomeDir+" -o check")
+}
+
+def help()
+{
+    println("This script creates a scriptfile for cms-shell to configure a jdbc connections, set upgrade parameters and do a 'upgrade check'")
+    println("Usage:")
+    println("upgradeCmdFromContextGenerator.groovy <context.xml> <cms.home dir>")
+    System.exit(2)
+}
+
+if ( args.length == 3 && new File(args[0]).isFile() && new File(args[1]).isDirectory() )
+{
+    contextParser(args[0], args[1], args[2])
+} else {
+    help()
+}
+
